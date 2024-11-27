8a047f81605f6faaf648ff1e0f8f4373e3e498e2
DivijManik <51446479+DivijManik@users.noreply.github.com>
2024-11-27 07:12:18 +0530

    problem4 - remove duplicates

--- c/Problem4.c
+++ c/Problem4.c
@@ -2,6 +2,25 @@
 
 int main()
 {
-    printf("test");
+    int arr[] = {1, 2, 2, 3, 4, 5, 5, 5, 6, 6};
+    int arrSize = (int)(sizeof(arr) / sizeof(arr[0]));
+    // printf("%d \n", arrSize);
+
+    int ans[10]; // this will be our final array
+    int ansSize = 0;
+    for (int i = 0; i < arrSize; i++)
+    {
+        /* code */
+        if (arr[i] != arr[i + 1])
+        {
+            ans[ansSize] = arr[i];
+            ansSize += 1;
+        }
+    }
+
+    for (int i = 0; i < ansSize; i++)
+    {
+        printf("%d \n", ans[i]);
+    }
     return 0;
 }
